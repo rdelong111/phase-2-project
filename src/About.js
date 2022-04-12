99d301126047aee310c2ce3f6d98259cc918e3be
Ryan DeLong <rdelong111@gmail.com>
2022-04-11 19:12:25 -0500

    add npm run server, tested react-router-dom capabilities

---
+++ src/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <h1>ABOUT TEST</h1>
+}
+
+export default About;
---
+++ src/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+function NavBar() {
+  return (
+    <div>
+      <NavLink
+        to="/"
+        exact
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+      >
+        About
+      </NavLink>
+    </div>
+  )
+}
+
+export default NavBar;
